0e529944d63441f1c758da0befd3236ebda0845b
ACHANGA EMMANUEL EBOT <achangaemmanuel2016@gmail.com>
2024-03-07 23:02:03 -0500

    code scan

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,6 +16,7 @@ public class Greeter {
    * This is a method.
    */
   public final String greet(final String someone) {
-    return String.format("Hello Avinash, %s!", someone);
+    string DB_PASSWORD = "Admin@45678";
+    return String.format("Hello Ebot, %s!", someone);
   }
 }
